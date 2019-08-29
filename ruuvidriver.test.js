a21c7c6eaca7c294ae1cf152b3548d1edc3a6a1c
Aarni Koskela <akx@iki.fi>
2019-08-29 12:54:30 +0300

    CircleCI: don't attempt initializing Ruuvidriver

--- ruuvidriver.test.js
+++ ruuvidriver.test.js
@@ -1,6 +1,9 @@
 describe("ruuvidriver", () => {
-  it("is initializable", () => {
+  it("is importable", () => {
     const rd = require(".");
+    if (process.env.CIRCLECI) {
+      return;  // there's no bluetooth hardware here
+    }
     rd.init();
     rd.stop();
   });
